8f4871b375d4e3ef55ef63b45fd103757cfd39b1
Joshua Ticer <Joshua.Ticer@umbrage.com>
2026-05-29 12:24:21 -0500

    added caching functionality

---
+++ app/cache.py
@@ -0,0 +1,20 @@
+import json
+from app.redis import redis_client
+
+async def get_cache(key: str):
+    value = await redis_client.get(key)
+    if value:
+        return json.loads(value)
+    return None
+
+
+async def set_cache(key: str, value, ttl: int = 300):
+    await redis_client.set(
+        key,
+        json.dumps(value),
+        ex=ttl
+    )
+
+
+async def delete_cache(key: str):
+    await redis_client.delete(key)
