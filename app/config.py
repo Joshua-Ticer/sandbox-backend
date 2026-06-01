4bb562e223a2c67ccc079ec3cc1159ca2492e3d1
Joshua Ticer <Joshua.Ticer@umbrage.com>
2026-06-01 10:29:14 -0500

    bugfixing

--- app/config.py
+++ app/config.py
@@ -5,15 +5,17 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
-    DATABASE_URL: str
-    REDIS_URL: str
+    POSTGRES_USER: str | None = None
+    POSTGRES_PASSWORD: str | None = None
+    POSTGRES_DB: str | None = None
+    DATABASE_URL: str 
+    REDIS_URL: str | None = None
+
+    ENV: str = "dev"
 
     model_config = SettingsConfigDict(
-        env_file=BASE_DIR / "project.env"
-    )
+        env_file=BASE_DIR / "project.env", 
+        extra="ignore")
 
 settings = Settings()
 
